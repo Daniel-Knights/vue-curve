7504febc797474096c8645bcf854d8161a2b50bc
Daniel Knights <danknights95@gmail.com>
2021-02-04 22:31:05 +0000

    Type declarations

--- src/lib/index.d.ts
+++ src/lib/index.d.ts
@@ -2,5 +2,13 @@ import type { Directive } from 'vue'
 
 declare module 'vue-curve'
 
+/**
+ * A directive for adding a curved box-shadow to any element through `v-curve`.
+ *
+ * ---
+ * **Options:**
+ * @property `color?` - `string`
+ * @property `intensity?` - `string` ("low", "medium" or "high")
+ */
 declare const VueCurve: Directive
 export default VueCurve
